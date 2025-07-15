f47e5e56e2660abdd66c74074226771e8b44021f
TOBENTOch <info@tobento.ch>
2025-07-15 11:53:26 +0200

    tests added

--- tests/Feature/Field/ItemsTest.php
+++ tests/Feature/Field/ItemsTest.php
@@ -26,6 +26,7 @@ use Tobento\Service\Storage\StorageInterface;
 class ItemsTest extends \Tobento\App\Crud\Test\Feature\TestCase
 {
     use \Tobento\App\Testing\Database\RefreshDatabases;
+    use \Tobento\App\Testing\FileStorage\RefreshFileStorages;
     
     public function createApp(): AppInterface
     {
@@ -55,6 +56,7 @@ class ItemsTest extends \Tobento\App\Crud\Test\Feature\TestCase
                 Field\Text::new('id'),
                 Field\Items::new('items')->fields(
                     Field\Text::new('price')->type('number')->validate('decimal'),
+                    Field\FileSource::new('filesrc')->allowedExtensions('jpg', 'txt'),
                 ),
             ],
             actions: [
@@ -63,6 +65,7 @@ class ItemsTest extends \Tobento\App\Crud\Test\Feature\TestCase
                 Action\Store::new(),
                 Action\Edit::new(),
                 Action\Update::new(),
+                Action\Delete::new(),
             ],
         );
     }
@@ -95,16 +98,56 @@ class ItemsTest extends \Tobento\App\Crud\Test\Feature\TestCase
     
     public function testUpdateActionDeletesItems()
     {
+        $fileStorage = $this->fakeFileStorage();
         $http = $this->fakeHttp();
         $http->request(method: 'PATCH', uri: $this->generateUpdateUri(id: 1))->body([
             'items' => [],
         ]);
         
-        $this->getSeedFactory(['items' => [1 => ['price' => '2.5']]])->times(1)->create();
-
+        $this->getSeedFactory(['items' => [1 => ['price' => '2.5', 'filesrc' => 'item1.txt']]])->times(1)->create();
+        $fileStorage->storage(name: 'uploads')->write(path: 'item1.txt', content: 'content');
+        
+        $this->assertTrue($fileStorage->storage(name: 'uploads')->exists(path: 'item1.txt'));
+        
         $http->response()->assertStatus(302)->assertLocation($this->generateIndexUri());
 
         $this->assertSame([], $this->getCrudRepository()->findById(1)->get('items'));
+        
+        $this->assertSame(0, count($fileStorage->storage(name: 'uploads')->files(path: '')->all()));
+        $this->assertFalse($fileStorage->storage(name: 'uploads')->exists(path: 'item1.txt'));
+    }
+    
+    public function testUpdateActionDeletesEmptyItems()
+    {
+        $fileStorage = $this->fakeFileStorage();
+        $http = $this->fakeHttp();
+        $http->request(method: 'PATCH', uri: $this->generateUpdateUri(id: 1))->body([
+            'items' => [
+                1 => ['price' => '1.7'],
+                2 => '',
+                3 => ['price' => '3.7']
+            ],
+        ]);
+        
+        $this->getSeedFactory([
+            'items' => [
+                1 => ['price' => '1.5'],
+                2 => ['price' => '2.5', 'filesrc' => 'item2.txt'],
+                3 => ['price' => '3.5'],
+            ]
+        ])->times(1)->create();
+        
+        $fileStorage->storage(name: 'uploads')->write(path: 'item2.txt', content: 'content');
+        
+        $this->assertTrue($fileStorage->storage(name: 'uploads')->exists(path: 'item2.txt'));
+
+        $http->response()->assertStatus(302)->assertLocation($this->generateIndexUri());
+
+        $this->assertSame(
+            [1 => ['price' => '1.7'], 2 => ['price' => '3.7']],
+            $this->getCrudRepository()->findById(1)->get('items'));
+        
+        $this->assertFalse($fileStorage->storage(name: 'uploads')->exists(path: 'item2.txt'));
     }
     
     public function testUpdateActionDeletesItemsIfNotArray()
