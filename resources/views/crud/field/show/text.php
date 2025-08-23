d965b3dfac17f70052289d6c98d03a5005536095
TOBENTOch <info@tobento.ch>
2025-08-23 13:04:41 +0200

    action name passed

--- resources/views/crud/field/show/text.php
+++ resources/views/crud/field/show/text.php
@@ -6,7 +6,7 @@
                 <div class="field">
                     <div class="field-label text-xxs"><?= $view->esc($name) ?></div>
                     <div class="field-body text-body">
-                        <?= $view->esc($field->getValue($field, $locale)) ?>
+                        <?= $view->esc($field->getValue(field: $field, locale: $locale, action: 'show')) ?>
                     </div>
                 </div>
             <?php } ?>
@@ -16,16 +16,7 @@
     <div class="field field-crud" data-field="<?= $view->esc($field->name()) ?>">
         <div class="field-label text-700"><?= $view->esc($field->label()) ?></div>
         <div class="field-body text-body">
-            <?php
-            $textValue = $field->getValue($field);
-            if ($field->getType() === 'date') {
-                echo $view->esc($view->date($textValue));
-            } elseif ($field->getType() === 'datetime-local') {
-                echo $view->esc($view->dateTime($textValue));
-            } else {
-                echo $view->esc($textValue);
-            }
-            ?>
+            <?= $view->esc($field->getValue(field: $field, action: 'show')) ?>
         </div>
     </div>
 <?php } ?>
