a32193506fdb450e63575f6fab6beed7449c53fc
TOBENTOch <info@tobento.ch>
2025-09-07 13:08:06 +0200

    padding added

--- resources/views/crud/field/files.php
+++ resources/views/crud/field/files.php
@@ -25,7 +25,7 @@ $form = $view->form();
             attributes: ['multiple', 'accept' => $field->acceptAttribute()],
         ) ?>
         <?php if ($field->getInfoText(action: $actionName)) { ?>
-            <p class="text-xxs"><?= $view->esc($field->getInfoText(action: $actionName)) ?></p>
+            <p class="text-xxs mt-xs"><?= $view->esc($field->getInfoText(action: $actionName)) ?></p>
         <?php } ?>
     </div>
 </div>
