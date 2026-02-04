5cf698d0823254534dccf4a981a56f78060ebbe3
TOBENTOch <info@tobento.ch>
2026-02-04 18:13:11 +0100

    supportedRequestMethods logic

--- src/Action/Traits/HandleNextAction.php
+++ src/Action/Traits/HandleNextAction.php
@@ -13,10 +13,12 @@ declare(strict_types=1);
 
 namespace Tobento\App\Crud\Action\Traits;
 
+use Psr\Http\Message\ResponseInterface;
 use Tobento\App\Crud\ActionProcessorInterface;
 use Tobento\App\Crud\Action\ActionInterface;
 use Tobento\App\Crud\Action\ActionsInterface;
 use Tobento\App\Crud\Entity\EntityInterface;
+use Tobento\Service\Requester\RequesterInterface;
 
 trait HandleNextAction
 {
@@ -27,31 +29,54 @@ trait HandleNextAction
      * @param ActionsInterface $actions
      * @param EntityInterface $entity
      * @param ActionProcessorInterface $actionProcessor
-     * @return void
+     * @return null|ResponseInterface
      */
     public function handleNextAction(
         ActionInterface $action,
         ActionsInterface $actions,
         EntityInterface $entity,
+        RequesterInterface $requester,
         ActionProcessorInterface $actionProcessor,
-    ): void {
+    ): null|ResponseInterface {
         $nextActionName = $action->getInput()->get('next_action');
-        
+
         if (!is_string($nextActionName)) {
-            return;
+            return null;
         }
-        
+
         [$actionName, $buttonName] = array_pad(explode('|', $nextActionName), 2, null);
-        
-        if (!is_null($nextAction = $actions->get(name: $actionName))) {
-            $nextAction->setEntity($entity);
-            $actionProcessor->resolveActionUrls(action: $nextAction);
-            $action->setLinkUrl($nextAction->getUrl());
-            
-            if ($buttonName && !is_null($button = $nextAction->buttons()->get($buttonName))) {
-                $url = $actionProcessor->urlResolver()->resolveButtonUrl($button, $nextAction, $entity);
-                $action->setLinkUrl($url);
-            }
+
+        $nextAction = $actions->get(name: $actionName);
+
+        if (is_null($nextAction)) {
+            return null;
         }
+
+        // Determine the method used for chaining
+        $method = $requester->method();
+
+        // If next action supports this method execute it
+        if (
+            !in_array($method, ['GET', 'HEAD', 'OPTIONS'])
+            && $nextAction->supportsRequestMethod($method)
+        ) {
+            // Execute next action and return its response
+            $nextAction->setActions($action->actions());
+            $nextAction->setController($action->controller());
+            
+            return $actionProcessor->call($nextAction->getHandler(), ['id' => $entity->id()]);
+        }
+
+        // Otherwise redirect to next action
+        $nextAction->setEntity($entity);
+        $actionProcessor->resolveActionUrls(action: $nextAction);
+        $action->setLinkUrl($nextAction->getUrl());
+
+        if ($buttonName && !is_null($button = $nextAction->buttons()->get($buttonName))) {
+            $url = $actionProcessor->urlResolver()->resolveButtonUrl($button, $nextAction, $entity);
+            $action->setLinkUrl($url);
+        }
+        
+        return null;
     }
 }
