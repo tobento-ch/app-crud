f68e74fa4f91259c7a4a8ff69550edb6cfc30570
TOBENTOch <info@tobento.ch>
2026-02-21 17:56:31 +0100

    fixed getValue

--- resources/views/crud/field/text-hidden.php
+++ resources/views/crud/field/text-hidden.php
@@ -6,7 +6,7 @@ if ($field->isTranslatable()) {
         echo $form->input(
             name: $field->name().'.'.$locale,
             type: $inputType,
-            value: $field->getValue($field, $locale),
+            value: $field->getValue($field, $locale, $actionName),
             attributes: $inputAttributes,
         );
     }
@@ -14,7 +14,7 @@ if ($field->isTranslatable()) {
     echo $form->input(
         name: $field->name(),
         type: $inputType,
-        value: $field->getValue($field),
+        value: $field->getValue($field, null, $actionName),
         attributes: $inputAttributes,
     );
 }
